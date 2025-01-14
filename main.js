ef853a6d67c72f4195616c4c8e804e42c0de53aa
tamaki_tech <oshaburimegane@gmail.com>
2025-01-14 15:29:55 +0900

    request to openai

--- main.js
+++ main.js
@@ -1,23 +1,52 @@
 // LINE developersのメッセージ送受信設定に記載のアクセストークン
 const LINE_TOKEN =
   PropertiesService.getScriptProperties().getProperty("LINE_TOKEN"); // Messaging API設定の一番下で発行できるLINE Botのアクセストークン
+const OPENAI_API_KEY =
+  PropertiesService.getScriptProperties().getProperty("OPENAI_API_KEY");
 const LINE_URL = "https://api.line.me/v2/bot/message/reply";
+const GPT_ENDPOINT = "https://api.openai.com/v1/chat/completions";
+const MODEL_NAME = "gpt-4o-mini";
+const MODEL_TEMP = 0.7;
+const MAX_TOKENS = 500;
+
+/**
+ * OpenAIへリクエストを投げてAIの解答を取得する
+ */
+const postOpenAI = (message) => {
+  // LINEのメッセージをChatGPTにメッセージ
+  const messages = [{ role: "user", content: message }];
+  const headers = {
+    Authorization: "Bearer " + OPENAI_API_KEY,
+    "Content-type": "application/json",
+  };
+  const options = {
+    method: "POST",
+    headers: headers,
+    payload: JSON.stringify({
+      model: MODEL_NAME, // 使用するGPTモデル
+      max_tokens: MAX_TOKENS, // 最大トークン
+      temperature: MODEL_TEMP, // ランダム性
+      messages: messages,
+    }),
+  };
+  const res = JSON.parse(
+    UrlFetchApp.fetch(GPT_ENDPOINT, options).getContentText()
+  );
+
+  return res.choices[0].message.content.trimStart();
+};
 
 //ユーザーがメッセージを送信した時に下記を実行する
-function doPost(e) {
+const doPost = (e) => {
   const json = JSON.parse(e.postData.contents);
-
-  //replyToken…イベントへの応答に使用するトークン(Messaging APIリファレンス)
-  // https://developers.line.biz/ja/reference/messaging-api/#message-event
-  const reply_token = json.events[0].replyToken;
-  const messageId = json.events[0].message.id;
+  const replyToken = json.events[0].replyToken;
   const messageType = json.events[0].message.type;
   const messageText = json.events[0].message.text;
 
-  // 検証で200を返すための取り組み
-  if (reply_token == null) {
-    return;
-  }
+  if (replyToken == null) return;
+  if (messageType !== "text") return;
+
+  const res = postOpenAI(messageText);
 
   const option = {
     headers: {
@@ -26,11 +55,11 @@ function doPost(e) {
     },
     method: "post",
     payload: JSON.stringify({
-      replyToken: reply_token,
+      replyToken: replyToken,
       messages: [
         {
           type: "text",
-          text: messageText,
+          text: res,
         },
       ],
     }),
@@ -39,4 +68,4 @@ function doPost(e) {
   UrlFetchApp.fetch(LINE_URL, option);
 
   return;
-}
+};
