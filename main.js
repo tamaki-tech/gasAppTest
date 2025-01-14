d3656df9a49c1fc705e25a759ed8de8ee4ed9e5b
tamaki_tech <oshaburimegane@gmail.com>
2025-01-14 14:14:04 +0900

    refactor

--- main.js
+++ main.js
@@ -1,39 +1,41 @@
- // LINE developersのメッセージ送受信設定に記載のアクセストークン
-const LINE_TOKEN = PropertiesService.getScriptProperties().getProperty("LINE_TOKEN"); // Messaging API設定の一番下で発行できるLINE Botのアクセストークン
-const LINE_URL = 'https://api.line.me/v2/bot/message/reply';
+// LINE developersのメッセージ送受信設定に記載のアクセストークン
+const LINE_TOKEN =
+  PropertiesService.getScriptProperties().getProperty("LINE_TOKEN"); // Messaging API設定の一番下で発行できるLINE Botのアクセストークン
+const LINE_URL = "https://api.line.me/v2/bot/message/reply";
 
 //ユーザーがメッセージを送信した時に下記を実行する
 function doPost(e) {
-
   const json = JSON.parse(e.postData.contents);
 
   //replyToken…イベントへの応答に使用するトークン(Messaging APIリファレンス)
   // https://developers.line.biz/ja/reference/messaging-api/#message-event
-  const reply_token = json.events[0].replyToken;
+  const replyToken = json.events[0].replyToken;
   const messageId = json.events[0].message.id;
   const messageType = json.events[0].message.type;
   const messageText = json.events[0].message.text;
 
-  // 検証で200を返すための取り組み
-  if (typeof reply_token === 'underfined') {
+  console.log(json.events[0]);
+
+  if (replyToken == null) {
     return;
   }
+
   const option = {
-    'headers': {
-      'Content-Type': 'application/json; charset=UTF-8',
-      'Authorization': 'Bearer ' + LINE_TOKEN,
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+      Authorization: `Bearer ${LINE_TOKEN}`,
     },
-    'method': 'post',
-    'payload': JSON.stringify({
-      'replyToken': reply_token,
-      'messages': [{
-        'type': 'text',
-        'text': messageText,
-      }],
+    method: "post",
+    payload: JSON.stringify({
+      replyToken: replyToken,
+      messages: [
+        {
+          type: "text",
+          text: messageText,
+        },
+      ],
     }),
-  }
+  };
 
-  UrlFetchApp.fetch(LINE_URL,option);
-
-  return;
+  UrlFetchApp.fetch(LINE_URL, option);
 }
